2f4d2431878ed14db4a26dfac8277e91f44059e0
benjamindeutsch <benjamin.deutsch@outlook.de>
2022-11-05 17:37:54 +0100

    Adding fillColor to triangle

--- draw.js
+++ draw.js
@@ -5,17 +5,29 @@ const PI = 3.14159265359;
 const two = new Two({
   type: Two.Types.canvas,
   fullscreen: true,
-  autostart: true
+  autostart: true,
 }).appendTo(document.body);
 
-function makeTriangle(two, a, b, c) {
+function makeTriangle(two, width, height, fillColor) {
   const group = two.makeGroup();
-  const base = two.makeLine(a.x, a.y, b.x, b.y);
+
+  const base = two.makeLine(0, 0, 0, height);
+  const line1 = two.makeLine(0, height, width, height / 2);
+  const line2 = two.makeLine(0, 0, width, height / 2);
+
+  //fill rectangle
+  if (fillColor) {
+    for (let i = 0; i < height; i += 0.5) {
+      const fillLine = two.makeLine(0, i, width, height / 2);
+      fillLine.stroke = fillColor;
+      fillLine.addTo(group);
+    }
+  }
+
   base.addTo(group);
-  const line1 = two.makeLine(b.x, b.y, c.x, c.y);
   line1.addTo(group);
-  const line2 = two.makeLine(a.x, a.y, c.x, c.y);
   line2.addTo(group);
+
   return group;
 }
 
@@ -27,39 +39,25 @@ function draw(size) {
   //head
   const headX = middleX + bodyWidth + size / 2;
   const headY = middleY - size;
+  const beak = makeTriangle(two, size, size / 2);
   two.makeCircle(headX, headY, size);
-  two.makeRectangle(headX + size / 2, headY - size / 2, size / 4, size / 4);
-  const beak = makeTriangle(
-    two,
-    { x: 0, y: 0 },
-    { x: 0, y: size / 2 },
-    { x: size, y: size / 4 }
-  );
-  beak.position.set(headX + size, headY - size / 4);
+  two.makeRectangle(headX + size / 3, headY - size / 3, size / 2, size / 2);
+  beak.position.set(headX + size - 5, headY - size / 4);
 
   //legs
-  two.makeRectangle(middleX - size * 2, middleY + size-5, size / 4, size / 2);
-  two.makeRectangle(middleX + size * 2, middleY + size-5, size / 4, size / 2);
+  two.makeRectangle(middleX - size * 2, middleY + size - 5, size / 4, size / 2);
+  two.makeRectangle(middleX + size * 2, middleY + size - 5, size / 4, size / 2);
 
   //wings and body
-  const wing2 = makeTriangle(
-    two,
-    { x: 0, y: 0 },
-    { x: 0, y: size * 2 },
-    { x: bodyWidth, y: size }
-  );
+  const wing2 = makeTriangle(two, bodyWidth, size * 2);
+
   wing2.position.set(middleX, middleY + size / 2);
   wing2.rotation = (-PI * 2) / 3;
 
   const body = two.makeEllipse(middleX, middleY, bodyWidth, size);
   body.fill = "#FFFFFF";
 
-  const wing1 = makeTriangle(
-    two,
-    { x: 0, y: 0 },
-    { x: 0, y: size * 2 },
-    { x: bodyWidth, y: size }
-  );
+  const wing1 = makeTriangle(two, bodyWidth, size * 2, "#FFFFFF");
   wing1.position.set(middleX, middleY - size / 2);
   wing1.rotation = -PI / 3;
 
@@ -71,5 +69,5 @@ function resize() {
   draw(window.innerWidth / 15);
 }
 
-two.bind('resize', resize);
+two.bind("resize", resize);
 resize();
